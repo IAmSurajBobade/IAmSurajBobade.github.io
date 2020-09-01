b7b1634fb6d2b49bc6fa51cc6fb426c39fe64af4
Suraj Bobade <iamsurajbobade@gmail.com>
2020-09-01 16:51:50 +0530

    Website redesign - initial

--- js/loadImg.js
+++ js/loadImg.js
@@ -1,6 +1,6 @@
-(function() {
-  (function($) {
-    return $.fn.imgPreload = function(options) {
+(function () {
+  (function ($) {
+    return $.fn.imgPreload = function (options) {
       var delay_completion, i, image_stack, placeholder_stack, replace, settings, spinner_stack, src, x, _i, _len;
       settings = {
         fake_delay: 10,
@@ -14,7 +14,7 @@
       placeholder_stack = [];
       spinner_stack = [];
       window.delay_completed = false;
-      delay_completion = function() {
+      delay_completion = function () {
         var x, _i, _len, _results;
         window.delay_completed = true;
         _results = [];
@@ -25,7 +25,7 @@
         return _results;
       };
       setTimeout(delay_completion, settings.fake_delay);
-      this.each(function() {
+      this.each(function () {
         var $image, $placeholder, $spinner_img, offset_left, offset_top;
         $image = $(this);
         offset_top = $image.offset().top;
@@ -48,7 +48,7 @@
           position: 'absolute'
         });
         $spinner_img.css('z-index', 9999);
-        $spinner_img.load(function() {
+        $spinner_img.load(function () {
           $(this).css({
             left: (offset_left + $placeholder.width() / 2) - $(this).width() / 2
           });
@@ -70,7 +70,7 @@
         x.attr({
           src: ''
         });
-        x.load(function() {
+        x.load(function () {
           if (window.delay_completed) {
             return replace(this);
           } else {
@@ -81,12 +81,12 @@
           src: src
         });
       }
-      replace = function(image) {
+      replace = function (image) {
         var $image, no_;
         $image = $(image);
         no_ = $image.attr('no');
         placeholder_stack[no_].replaceWith($image);
-        spinner_stack[no_].fadeOut(settings.animation_duration / 2, function() {
+        spinner_stack[no_].fadeOut(settings.animation_duration / 2, function () {
           return $(this).remove();
         });
         return $image.fadeOut(0).fadeIn(settings.animation_duration);
